7c3133dbc47ecddca9898b9ed9b14a2a418f1c0f
Aaron Wolf <aaron.wolf@gmail.com>
2024-05-12 21:12:40 +0300

    backend

---
+++ backend/api/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+# import models
+from .models import Note
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password:": {"write_only": True}}
+
+    def create(self, valideted_data):
+        user = User.objects.create_user(**valideted_data)
+        return user
+    
+# The model to be served as JSON
+class NoteSerializer(serializers.ModeSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}

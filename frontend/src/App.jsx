a2ccf02dbe6e00becc4aa37a566bccc181917d2f
Aaron Wolf <aaron.wolf@gmail.com>
2024-05-15 12:46:05 +0300

    all components set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,41 @@
 import react from 'react'
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
+import ProtectedRoute from './components/ProtectedRoute'
 // import './App.css'
 
+function Logout() {
+  localStorage.clear()
+  return <Navigate to='/login' />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/"
+          // most of the time Home would not be protected. 
+          // It's protected here for the demo.
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
